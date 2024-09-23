04ea88e5281fd0fc49fb7aeb74f1e24ce5239e55
MermoudYann <yann.mermoud@heig-vd.ch>
2024-09-23 13:45:24 +0200

    1st update

---
+++ 01 - Introduction/02-01-code_C++.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>                         // 1
+#include <cstdlib>
+using namespace std;                        // 2
+
+int main()                                  // 3
+{                                           // 4
+    cout << "Hello world"       << endl;    // 5
+    cout << "fin de programme"  << endl;
+    return EXIT_SUCCESS;                    // 6
+}                                           // 4
+
