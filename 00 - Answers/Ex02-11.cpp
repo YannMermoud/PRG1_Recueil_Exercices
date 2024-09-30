cb4beae46561344108513b498ecadd37c91119f9
MermoudYann <yann.mermoud@heig-vd.ch>
2024-09-30 14:08:19 +0200

    Ex 02-11 résolu

---
+++ 00 - Answers/Ex02-11.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    unsigned long hauteur = 0;
+
+    do {
+        cout << "Hauteur du triangle (h > 0) : ";
+        if (cin >> hauteur and hauteur != 0) {
+            break;
+        }
+    } while (true);
+
+    cout << endl;
+
+    for (unsigned long x(0); x < hauteur; ++x) {
+        for (unsigned long y(0); y < 2 * hauteur - 1; ++y) {
+            if ((y < hauteur - 1 - x) or
+                (y > hauteur - 1 + x)) {
+                cout << ".";
+            } else {
+                cout << "*";
+            }
+        }
+        cout << "\n";
+    }
+}
