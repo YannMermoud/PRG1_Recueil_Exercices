bdd4aceb8ba4f62bfd2a6b950318abe3486778b4
MermoudYann <yann.mermoud@heig-vd.ch>
2024-10-08 09:27:36 +0200

    exercice 04-07-02 solved

--- 00 - Answers/Ex04-07-02.cpp
+++ 00 - Answers/Ex04-07-02.cpp
@@ -4,12 +4,19 @@ using namespace std;
 
 void compteur() {
     static int counter(0);
-    cout << ++counter << endl;
+    cout << "Appel No : " << ++counter << endl;
 }
+void compteur(int& counter) { cout << "Appel No : " << ++counter << endl; }
 
 int main() {
+    int counter(0);
     compteur();
     compteur();
     compteur();
+
+    compteur(counter);
+    compteur(counter);
+    compteur(counter);
+
     return 0;
 }
